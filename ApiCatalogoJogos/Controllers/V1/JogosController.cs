78178687fd60fead6f38310c4b6befff8cb3153d
Suiany <suianypatricia@gmail.com>
2021-09-25 00:47:02 -0300

    Os arquivos estão sendo adicionados ao repositório.

---
+++ ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,129 @@
+using ApiCatalogoJogos.Exceptions;
+using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.Services;
+using ApiCatalogoJogos.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Controller.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        // GET, POST, PUT, PATCH, DELETE
+
+        private readonly IJogoService _jogoService;
+
+        public JogosController(IJogoService jogoService)
+        {
+            _jogoService = jogoService;
+        }
+
+        /// <summary>
+        /// Buscar todos os jogos de forma paginada
+        /// </summary>
+        /// <remarks>
+        /// Não é possível retornar os jogos sem paginação
+        /// </remarks>
+        /// <param name="pagina">Indica qual página está sendo consultada. Mínimo 1</param>
+        /// <param name="quantidade">Indica a quantidade de reistros por página. Mínimo 1 e máximo 50</param>
+        /// <response code="200">Retorna a lista de jogos</response>
+        /// <response code="204">Caso não haja jogos</response>  
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
+        {
+            throw new Exception();
+
+            var jogos = await _jogoService.Obter(pagina, quantidade);
+
+            if (jogos.Count() == 0)
+                return NoContent();
+
+            return Ok(jogos);
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<JogoViewModel>> Obter([FromRoute] Guid idJogo)
+        {
+            var jogo = await _jogoService.Obter(idJogo);
+
+            if (jogo == null)
+                return NoContent();
+
+            return Ok(jogo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody] JogoInputModel jogoInputModel)
+        {
+            try
+            {
+                var jogo = await _jogoService.Inserir(jogoInputModel);
+
+                return Ok(jogo);
+            }
+            catch (JogoJaCadastradoException ex)
+            {
+                return UnprocessableEntity("Já existe um jogo com este nome para esta produtora");
+            }
+            
+        }
+
+        [HttpPut("{idJogo:guid}")] // PUT atualiza o recurso inteiro (nome, preço, etc)
+        public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo,[FromBody] JogoInputModel jogoInputModel)
+        {
+            try
+            {
+                await _jogoService.Atualizar(idJogo, jogoInputModel);
+
+                return Ok();
+            }
+            catch (JogoNaoCadastradoException ex)
+            {
+                return NotFound("Este jogo não existe");
+            }
+            
+        }
+
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")] // PATCH atualiza um atributo específico (preço)
+        public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo,[FromRoute] double preco)
+        {
+            try
+            {
+                await _jogoService.Atualizar(idJogo, preco);
+
+                return Ok();
+            }
+            catch (JogoNaoCadastradoException ex)
+            {
+                return NotFound("Este jogo não existe");
+            }
+            
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> ApagarJogo([FromRoute] Guid idJogo)
+        {
+            try
+            {
+                await _jogoService.Remover(idJogo);
+
+                return Ok();
+            }
+            catch (JogoNaoCadastradoException ex)
+            {
+                return NotFound("Este jogo não existe");
+            }
+            
+        }
+
+
+    }
+}
---
+++ ApiCatalogoJogos/InputModel/JogoInputModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.InputModel
+{
+
+    public class JogoInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do jogo deve conter entre 3 e 100 caracteres")]
+
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome da produtora deve conter entre 3 e 100 caracteres")]
+
+        public string Produtora { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "OO preço deve ser no mínimo R$ 1,00 e no máximo R$ 1.000,00")]
+
+        public double Preco { get; set; }
+    }
+}
